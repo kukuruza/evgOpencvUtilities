18ddf57e92a105b17dab5eb0f0581b69094e2ead
Evgeny Toropov <etoropov@andrew.cmu.edu>
2014-12-13 11:56:46 -0500

    adds epipolar, two-view and three-view (general case) constraints to multiview.h

--- src/multiview.cpp
+++ src/multiview.cpp
@@ -14,6 +14,21 @@ namespace evg {
 using namespace std;
 using namespace boost;
 
+
+inline Matx31f cross (const Matx31f& a, const Matx31f& b)
+{
+    return Matx31f (a(1)*b(2)-a(2)*b(1),
+                    a(2)*b(0)-a(0)*b(2),
+                    a(0)*b(1)-a(1)*b(0));
+}
+
+inline float dot (const Matx31f& a, const Matx31f& b)
+{
+    return a.dot(b);
+}
+
+
+
 // calculate essential matrix
 optional<Matx33f> findEssentialMat(const Mat& p1, const Mat& p2, float FundEstErrCutoff, float SRatioCutoff)
 {
@@ -60,6 +75,65 @@ cv::Matx44f E2pose (const cv::Matx33f& E12)
     Matx44f pose2 = pose(R, t);
     return pose2;
 }
+
+
+cv::Matx33f pose2E (const cv::Matx44f& pose)
+{
+    Matx33f R = decomPose2R(pose);
+    Matx31f t = decomPose2t(pose);
+    return skewsym(t) * R;
+}
+
+
+float epipolarError (const Matx44f& pose2, const Matx31f& p1, const Matx31f& p2)
+{
+    cv::Matx<float, 1, 1> error = p2.t() * pose2E(pose2) * p1;
+    return std::abs(error(0));
+}
+
+
+float twoViewError (const Matx44f& pose2, const Matx31f& p1, const Matx31f& p2)
+{
+    Matx33f R2 = decomPose2R(pose2);
+    Matx31f t2 = decomPose2t(pose2);
+
+    Matx31f q1 = p1;
+    Matx31f q2 = R2.t() * p2;
+    return dot (q1, cross(t2, q2));
+}
+
+
+vector<float> threeViewErrors (const Matx44f& pose2, const Matx44f& pose3,
+                               const Matx31f& p1, const Matx31f& p2, const Matx31f& p3)
+{
+    Matx33f R2 = decomPose2R(pose2);
+    Matx31f t2 = decomPose2t(pose2);
+    Matx33f R3 = decomPose2R(pose3);
+    Matx31f t3 = decomPose2t(pose3);
+
+    Matx31f q1 = p1;
+    Matx31f q2 = R2.t() * p2;
+    Matx31f q3 = R3.t() * p3;
+    
+    Matx31f t1to2 = t2;
+    Matx31f t2to3 = t3 - t2;
+    
+    //TODO: case of three camera centers on one line. Then the return vector will have 4 elements
+    
+    float g1 = dot (q1, cross(t1to2, q2));
+    float g2 = dot (q2, cross(t2to3, q3));
+    float g3 = dot (cross(q2, q1), cross(q3, t2to3))
+             - dot (cross(q1, t1to2), cross(q3, q2));
+    
+    Mat result (Matx31f(std::abs(g1), std::abs(g2), std::abs(g3)));
+    return vector<float> (result.ptr<float>(0), result.ptr<float>(3));
+}
+
+
+
+
+
+
 
 
 }
--- src/multiview.h
+++ src/multiview.h
@@ -14,6 +14,22 @@ boost::optional<cv::Matx33f> findEssentialMat (const cv::Mat& p1, const cv::Mat&
 
 
 cv::Matx44f E2pose (const cv::Matx33f& E12);
+cv::Matx33f pose2E (const cv::Matx44f& pose);
+
+
+
+float epipolarError (const Matx44f& pose2, const Matx31f& p1, const Matx31f& p2);
+
+
+// two-view and three-view constraints of V.Indelman
+// pose2, pose3 - is the pose of the 2nd and 3rd cameras in the frame of the 1st camera
+// p1, p2, p3 - projections of a 3D point into 1st, 2nd, and 3rd cameras
+
+float twoViewError (const cv::Matx44f& pose2, const cv::Matx31f& p1, const cv::Matx31f& p2);
+
+std::vector<float>
+threeViewErrors (const cv::Matx44f& pose2, const cv::Matx44f& pose3,
+                 const cv::Matx31f& p1, const cv::Matx31f& p2, const cv::Matx31f& p3);
 
 }
 }
